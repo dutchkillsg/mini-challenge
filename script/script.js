bc85301dfe7f304e919246ce2b89dc0913898dbc
dutchkillsg <mip12793@gmail.com>
2017-07-17 11:32:26 -0400

    Mini Challenge

---
+++ script/script.js
@@ -0,0 +1,30 @@
+/*document.getElementById("right").addEventListener("click", function(){
+    alert("I am Right!");
+});
+document.getElementById("wrong").addEventListener("click", function(){
+    alert("No,I'm Right!");
+});
+
+document.getElementById("dare").addEventListener("mouseover", function(){
+	alert("I fucking told you not to hover over me!!");*/
+
+/*});*/
+
+window.addEventListener("keypress",function(e)) {
+    var evalue = e.key
+    document.getElementById("press").innerHtml=e.key
+}
+
+
+
+//question 4
+document.getElementById('frm').addEventListener('submit',function(e)){
+	var us = document.getElementById("user");
+    var ps = document.getElementById("pass");
+    if (us == (?=.*\d) && ps == "12345678") {
+        console.log("you are in")
+      } else {
+        return "incorrect"
+      }
+ }
+
